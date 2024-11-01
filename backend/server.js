afc351f0186c7eb81b19e5cba981805c10b9e312
Nyttho <anthony.dms@outlook.com>
2024-11-01 17:28:40 +0100

    connection to mongoDb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,27 @@
+require("dotenv").config();
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+const uri = process.env.MONGODB_URI;
+
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
 
 app.listen(3000);
